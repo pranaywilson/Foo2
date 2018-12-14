29279673385e6d3eeb19403fbd324d87e19049b5
Pranay Tigga <pranay.tigga@gmail.com>
2018-12-14 09:12:56 -0800

    comit

--- SecurityFramework/AspNetSecurity-m4-Shared/Models/AttendeeModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AspNetSecurity_m4_Shared.Models
-{
-    public class AttendeeModel
-    {
-        public int Id { get; set; }
-        public int ConferenceId { get; set; }
-        public string Name { get; set; }
-    }
-}
